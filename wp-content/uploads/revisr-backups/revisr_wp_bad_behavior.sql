01c9344add0b8c8102c005eba598e54c1838f876
admin <jdbradbury@gmail.com>
2015-09-10 05:01:23 +0000

    Backed up the database with Revisr.

--- wp-content/uploads/revisr-backups/revisr_wp_bad_behavior.sql
+++ wp-content/uploads/revisr-backups/revisr_wp_bad_behavior.sql
@@ -26,11 +26,12 @@ CREATE TABLE `wp_bad_behavior` (
   PRIMARY KEY (`id`),
   KEY `ip` (`ip`(15)),
   KEY `user_agent` (`user_agent`(10))
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `wp_bad_behavior` WRITE;
 /*!40000 ALTER TABLE `wp_bad_behavior` DISABLE KEYS */;
+INSERT INTO `wp_bad_behavior` VALUES (1,'93.174.93.33','2015-09-06 09:08:01','GET','/','HTTP/1.0','GET / HTTP/1.0\n','','','00000000'),(2,'182.118.53.164','2015-09-06 18:34:54','GET','/','HTTP/1.1','GET / HTTP/1.1\nHost: 52.5.79.162\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2251.0 Safari/537.36\nAccept-Encoding: gzip\n','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2251.0 Safari/537.36','','17566707'),(3,'93.174.93.33','2015-09-07 10:44:58','GET','/','HTTP/1.0','GET / HTTP/1.0\n','','','00000000'),(4,'183.60.48.25','2015-09-07 13:29:54','GET','http://www.baidu.com/','HTTP/1.1','GET http://www.baidu.com/ HTTP/1.1\nHost: www.baidu.com\nAccept: */*\nContent-Type: text/html\nProxy-Connection: Keep-Alive\nContent-length: 0\n','','','00000000'),(5,'137.226.113.7','2015-09-08 15:48:16','GET','/','HTTP/1.1','GET / HTTP/1.1\nHost: 52.5.79.162\nConnection: Keep-Alive\n','','','00000000'),(6,'182.118.55.234','2015-09-08 18:58:08','GET','/','HTTP/1.1','GET / HTTP/1.1\nHost: 52.5.79.162\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2251.0 Safari/537.36\nAccept-Encoding: gzip\n','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2251.0 Safari/537.36','','17566707'),(7,'137.226.113.7','2015-09-08 22:15:15','GET','/','HTTP/1.1','GET / HTTP/1.1\nHost: 52.5.79.162\nConnection: Keep-Alive\n','','','00000000'),(8,'141.212.121.144','2015-09-09 01:58:21','GET','/','HTTP/1.1','GET / HTTP/1.1\nHost: 52.5.79.162\nUser-Agent: Mozilla/5.0 zgrab/0.x\n','Mozilla/5.0 zgrab/0.x','','17566707'),(9,'141.212.122.50','2015-09-10 03:06:01','CONNECT','proxytest.zmap.io:80','HTTP/1.1','CONNECT proxytest.zmap.io:80 HTTP/1.1\nHost: 52.5.79.162\nUser-Agent: Mozilla/5.0 zgrab/0.x\n','Mozilla/5.0 zgrab/0.x','','17566707');
 /*!40000 ALTER TABLE `wp_bad_behavior` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
