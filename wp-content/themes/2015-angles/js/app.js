6460ee2899d10286c4b94a15e69f67ebfa50627a
admin <jdbradbury@gmail.com>
2015-09-12 22:47:35 +0000

    Reverted to commit: #6cd9142.

--- wp-content/themes/2015-angles/js/app.js
+++ wp-content/themes/2015-angles/js/app.js
@@ -9,20 +9,13 @@ angles.run( ['$rootScope', function($rootScope) {
 }]);
 
 // Add a controller
-angles.controller( 'wordpress', ['$scope', '$sce', '$http', function( $scope, $sce, $http ) {
-  $scope.watchLast = 0;  
+angles.controller( 'wordpress', ['$scope', '$sce', '$http', function( $scope, $sce, $http ) { 
   
   // trust html method
   $scope.trust = function (html) { return $sce.trustAsHtml(html); };
   
   // get post(s) method
   $scope.getPost = function ( id ) {
-    $scope.watchDiff = Date.now() - $scope.watchLast;
-    if($scope.watchDiff < 1000 ){
-      //console.log('too soon');
-      return false;
-    }
-    //console.log( $scope.watchDiff );
     var param = {};
     var theurl = $scope.apiuri + 'get_posts';
     var single = false;
@@ -52,7 +45,6 @@ angles.controller( 'wordpress', ['$scope', '$sce', '$http', function( $scope, $s
       	$scope.posts = data.posts; // return 'posts' array
       }
       //console.log( $scope.posts );
-      $scope.watchLast = Date.now();
     }).
     error(function(data, status, headers, config) {
       // bail to 404
