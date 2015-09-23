baed7088ab71b5d15ef8644538daecfba4b3dcc1
admin <jdbradbury@gmail.com>
2015-09-23 16:32:13 +0000

    add full site

--- wp-content/uploads/revisr-backups/revisr_wp_terms.sql
+++ wp-content/uploads/revisr-backups/revisr_wp_terms.sql
@@ -20,12 +20,12 @@ CREATE TABLE `wp_terms` (
   PRIMARY KEY (`term_id`),
   KEY `slug` (`slug`(191)),
   KEY `name` (`name`(191))
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `wp_terms` WRITE;
 /*!40000 ALTER TABLE `wp_terms` DISABLE KEYS */;
-INSERT INTO `wp_terms` VALUES (1,'Uncategorized','uncategorized',0),(2,'Becoming a WordPress Ninja','becoming-a-wordpress-ninja',0);
+INSERT INTO `wp_terms` VALUES (2,'Becoming a WordPress Ninja','becoming-a-wordpress-ninja',0),(3,'Urban','urban',0),(4,'Modern','modern',0),(5,'Rustic','rustic',0),(6,'Status Quo','status-quo',0),(7,'So Over It','so-over-it',0),(8,'Classic','classic',0);
 /*!40000 ALTER TABLE `wp_terms` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
